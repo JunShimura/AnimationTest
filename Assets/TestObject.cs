3a7c5142c86486028587d1b71997d4d437ac57b7
Junshimura <junshimura@gmail.com>
2018-09-17 13:24:17 +0900

    Initial commit

---
+++ Assets/TestObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestObject : MonoBehaviour {
+
+    Animator testAnimator;
+	// Use this for initialization
+	void Start () {
+        testAnimator = transform.GetChild(0).gameObject.GetComponent<Animator>();
+	}
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            testAnimator.SetTrigger("TestStart");
+        }
+	}
+}
